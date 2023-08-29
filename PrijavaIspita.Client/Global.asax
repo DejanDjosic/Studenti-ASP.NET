9313dec08004ce6aa5e589cd35045295ab28fb7e
DejanDjosic <ddjosic@gmail.com>
2023-08-29 18:55:14 +0200

    first commit

---
+++ PrijavaIspita.Client/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PrijavaIspita.Client.MvcApplication" Language="C#" %>
---
+++ PrijavaIspita.Domain/StudentBo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrijavaIspita.Domain
+{
+   public class StudentBo
+    {
+        public int Id { get; set; }
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public int GodinaUpisa { get; set; }
+
+        public SmerBo Smer { get; set; }
+    }
+}
